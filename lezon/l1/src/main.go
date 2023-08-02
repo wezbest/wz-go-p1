ab02d571a98f66d186e80789beace85e37de18bd
wezbest <110793713+wezbest@users.noreply.github.com>
2023-08-02 06:17:07 +0000

    .

---
+++ lezon/l1/src/main.go
@@ -0,0 +1,15 @@
+/*
+This was made by you for your work
+*/
+
+package main 
+
+import (
+	// f "fmt"
+	l "l1/lib" 
+)
+
+func main() {
+	l.ColoBanner("Lesson 1 - Datatypes")
+	l.Headr("Making my own functions")
+}
